b4f2ff1afc5d3b616176f25d5bda434dbf52384d
Jonatan Rinckus <jonatan@bedatse.com>
2017-11-17 13:16:17 -0300

    aula 69

--- src/containers/weather_list.jsx
+++ src/containers/weather_list.jsx
@@ -1,23 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Sparklines, SparklinesLine } from "react-sparklines";
+import Chart from "../components/chart"
+import GoogleMap from "../components/google_map"
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
         const temps = cityData.list.map(w => w.main.temp);
-        const humiditys = cityData.list.map(w => w.main.humidity);
+        const humidities = cityData.list.map(w => w.main.humidity);
         const pressures = cityData.list.map(w => w.main.pressure);
         return (
             <tr key={name}>
                 <td>
-                    {name}
+                    <GoogleMap lat={cityData.city.coord.lat} lon={cityData.city.coord.lon}/>
                 </td>
                 <td>
-                    <Sparklines height={120} width={180} data={temps} >
-                        <SparklinesLine color="red" />
-                    </Sparklines>
+                    <Chart data={temps} units="K" color={"red"}/>
                 </td>
+                
+                <td>
+                    <Chart data={pressures} units="hPa" color={"green"}/>
+                </td>
+
+                <td>
+                    <Chart data={humidities} color={"black"} units="%"/>
+                </td>
+                
             </tr>
         )
     }
@@ -28,9 +36,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
