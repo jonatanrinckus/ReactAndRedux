8a222c3431d1770001fd8cddc8a368f7bc580600
Jonatan Rinckus <jonatan@loop3.com.br>
2017-11-17 17:26:49 -0200

    s

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import WeatherReducer from "./reducer_weather";
+
 const rootReducer = combineReducers({
-  weather: WeatherReducer
+  state: (state = {}) => state
 });
 
 export default rootReducer;
